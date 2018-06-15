2699f787b48bf93aaec5141d8f0fbf96266a3e23
Amber Ostendorf <ambersed_2000@yahoo.com>
2018-06-15 10:28:27 -0600

    initial commit

---
+++ letters.js
@@ -0,0 +1,14 @@
+//take in a string
+//make lowercase
+//spread the words into an array of letters
+//make a hash to keep track of the letter & instances
+//loop each array against the other
+//if duplicate 
+
+function findduplicateletters(string) {
+  string = "this is a Test"
+  string.toLowercase
+  let array = [...string]
+  for (var i=0; i<string.length; i++)
+  
+}
